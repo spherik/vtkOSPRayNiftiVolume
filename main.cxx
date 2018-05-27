75b903f1d63574b2beb21b7b90fece8b3adcbd7b
Toni Gurgui <toni.gurgui@gmail.com>
2018-05-27 19:50:10 +0200

    * Modified transfer functions * Write output vtk image (optional)

--- main.cxx
+++ main.cxx
@@ -21,14 +21,26 @@
 #include <vtkNIFTIImageHeader.h>
 #include <vtkImageImport.h>
 #include <vtkInteractorStyleTrackballCamera.h>
+#include <vtkXMLImageDataWriter.h>
 
 int main(int argc, char *argv[])
 {
   bool useOSP = true;
-
+  bool writeVTI = false;
   vtkNew<vtkNIFTIImageReader> reader;
-  reader->SetFileName("/Volumes/2T/BrainData/tutorial_epimap/anat/MRI/3DT1post_deface.nii");
+  reader->SetFileName("/Volumes/2T/BrainData/mindboggle/example_mri_data/T1.nii.gz");
   reader->Update();
+
+  vtkNIFTIImageHeader* header = reader->GetNIFTIHeader();
+  std::cout << header->GetDataType() << std::endl;
+
+  if(writeVTI)
+  {
+    vtkNew<vtkXMLImageDataWriter> writer;
+    writer->SetFileName("output.vti");
+    writer->SetInputData(reader->GetOutput());
+    writer->Write();
+  }
 
   vtkNew<vtkGPUVolumeRayCastMapper> volumeMapper;
   volumeMapper->SetAutoAdjustSampleDistances(0);
@@ -37,17 +49,27 @@ int main(int argc, char *argv[])
 
   vtkNew<vtkVolumeProperty> volumeProperty;
   vtkNew<vtkColorTransferFunction> ctf;
-  ctf->AddRGBPoint(37.3531, 0.2, 0.29, 1);
-  ctf->AddRGBPoint(157.091, 0.87, 0.87, 0.87);
-  ctf->AddRGBPoint(276.829, 0.7, 0.015, 0.15);
+  ctf->AddRGBPoint(-32768, 0.0, 0., 0.);
+  ctf->AddRGBPoint(-28414, 1.,0.8,0.4);
+  ctf->AddRGBPoint(-20806, 0.92549,0.917647,0.611765);
+  ctf->AddRGBPoint(-14365, 0.901961,1.,0.772549);
+  ctf->AddRGBPoint(17840, 1,0.,0.);
+  ctf->AddRGBPoint(32767, 1.,0,0);  //
 
   vtkNew<vtkPiecewiseFunction> pwf;
-  pwf->AddPoint(37.3531, 0.0);
-  pwf->AddPoint(276.829, 1.0);
+  pwf->AddPoint(-32768, 0.0);
+  pwf->AddPoint(-30476.6, 0.0);
+  pwf->AddPoint(-19068, 0.5875);
+  pwf->AddPoint(-17227.7, 0.16875);
+  pwf->AddPoint(-13956.1, 0.24375);
+  pwf->AddPoint(3833.45, 0.775);
+  pwf->AddPoint(9149.86, 0.79375);
+  pwf->AddPoint(32767, 0);
+
 
   volumeProperty->SetColor(ctf);
   volumeProperty->SetScalarOpacity(pwf);
-  volumeProperty->SetShade(1);
+  volumeProperty->SetShade(0);
   volumeProperty->SetScalarOpacityUnitDistance(1.732);
 
   vtkNew<vtkVolume> volume;
@@ -57,7 +79,7 @@ int main(int argc, char *argv[])
   // Create the renderwindow, interactor and renderer
   vtkNew<vtkRenderWindow> renderWindow;
   renderWindow->SetMultiSamples(0);
-  renderWindow->SetSize(401, 399); // NPOT size
+  renderWindow->SetSize(1024, 768); // NPOT size
   vtkNew<vtkRenderWindowInteractor> iren;
   iren->SetRenderWindow(renderWindow);
   vtkNew<vtkInteractorStyleTrackballCamera> style;
